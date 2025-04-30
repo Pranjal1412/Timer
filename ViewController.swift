660a5649617a454bcb6f5a6612f5a33376e9c9c9
pranjal <shinde.pc1707@gmail.com>
2025-04-30 11:05:05 +0530

    Final Project

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    
+    let eggTime = ["Soft":10, "Medium":20, "Hard":30]
+    var secondsRemaining = 0
+    var timer = Timer()
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        self.secondsRemaining = eggTime[sender.currentTitle!]!
+        timer.invalidate()
+        textDisplay.text = "How do you like your eggs?"
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
+                (Timer) in
+                if (self.secondsRemaining > 0) {
+                    print ("\(self.secondsRemaining) seconds")
+                    self.secondsRemaining -= 1
+                }
+                else {
+                    Timer.invalidate()
+                    self.textDisplay.text = "Time Up!"
+                }
+        }
+
+    }
+    
+   
+
+}
